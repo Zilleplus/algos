8584fff589c74ddb30075b5e0cdc02698ba307e4
zilleplus <zilleplus@gmail.com>
2021-09-05 17:16:44 +0200

    Add disjoint implementation from chapter5

---
+++ AADS/chapter5/disjointSet.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self):
+        self.parent = None
+        self.rank = 1  # only updated for root nodes
+
+
+class DisjointSet:
+    def __init__(self):
+        self.nodes_ = {}
+        self.last_id_ = 0
+
+    def AddElement(self, element):
+        self.nodes_[element] = Node()
+
+    def FindPartition(self, n: Node):
+        if(n.parent is None):
+            return n
+
+        root = self.FindPartition(n.parent)
+        # while we backtrack our traces, set
+        # this root to the parent root, shortcutting
+        # for the next call
+        if(n.parent != root):
+            n.parent = root
+
+        return root
+
+    def Merge(self, element_x, element_y):
+        x = self.nodes_[element_x]
+        y = self.nodes_[element_y]
+
+        p_x = self.FindPartition(x)
+        p_y = self.FindPartition(y)
+
+        new_rank = p_x.rank + p_y.rank
+        if(p_x.rank < p_y.rank):
+            p_y.parent = p_x
+            p_x.rank = new_rank
+        else:
+            p_x.parent = p_y
+            p_y.rank = new_rank
+
+    def AreDisjoint(self, element_x, element_y):
+        x = self.nodes_[element_x]
+        y = self.nodes_[element_y]
+
+        p_x = self.FindPartition(x)
+        p_y = self.FindPartition(y)
+
+        return not p_x == p_y
+
+
+d = DisjointSet()
+d.AddElement('a')
+d.AddElement('b')
+d.AddElement('c')
+
+d.Merge('a', 'b')
+print(d.AreDisjoint('a', 'b'))
