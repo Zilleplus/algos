92d257acfc78ff07fec61d28699adf521c57c300
zilleplus <zillepus@gmail.com>
2023-04-01 09:31:14 +0200

    Review disjointSet

--- AADS/chapter5/disjointSet.py
+++ AADS/chapter5/disjointSet.py
@@ -1,31 +1,40 @@
+from typing import List, Optional
+
+
 class Node:
+    parent: Optional[Node]
+    rank: int
+
     def __init__(self):
         self.parent = None
         self.rank = 1  # only updated for root nodes
 
 
 class DisjointSet:
+    nodes_: dict[str, Node]
+    last_id_: int
+
     def __init__(self):
         self.nodes_ = {}
         self.last_id_ = 0
 
-    def AddElement(self, element):
+    def AddElement(self, element: str):
         self.nodes_[element] = Node()
 
     def FindPartition(self, n: Node):
-        if(n.parent is None):
+        if n.parent is None:
             return n
 
         root = self.FindPartition(n.parent)
         # while we backtrack our traces, set
         # this root to the parent root, shortcutting
         # for the next call
-        if(n.parent != root):
+        if n.parent != root:
             n.parent = root
 
         return root
 
-    def Merge(self, element_x, element_y):
+    def Merge(self, element_x: str, element_y: str):
         x = self.nodes_[element_x]
         y = self.nodes_[element_y]
 
@@ -33,14 +42,14 @@ class DisjointSet:
         p_y = self.FindPartition(y)
 
         new_rank = p_x.rank + p_y.rank
-        if(p_x.rank < p_y.rank):
+        if p_x.rank < p_y.rank:
             p_y.parent = p_x
             p_x.rank = new_rank
         else:
             p_x.parent = p_y
             p_y.rank = new_rank
 
-    def AreDisjoint(self, element_x, element_y):
+    def AreDisjoint(self, element_x: str, element_y: str):
         x = self.nodes_[element_x]
         y = self.nodes_[element_y]
 
@@ -51,9 +60,9 @@ class DisjointSet:
 
 
 d = DisjointSet()
-d.AddElement('a')
-d.AddElement('b')
-d.AddElement('c')
+d.AddElement("a")
+d.AddElement("b")
+d.AddElement("c")
 
-d.Merge('a', 'b')
-print(d.AreDisjoint('a', 'b'))
+d.Merge("a", "b")
+print(d.AreDisjoint("a", "b"))
